9a43c611b8d488e5374d6ad1454c3a988887583a
PrathamPatel2004 <137031619+PrathamPatel2004@users.noreply.github.com>
2025-07-15 07:26:33 +0530

    Home Page CategorySlide

--- Frontend/src/Component/CategorySlide.jsx
+++ Frontend/src/Component/CategorySlide.jsx
@@ -1,18 +1,17 @@
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
-import { FreeMode, Pagination } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 import member2 from '../assets/member2.jpg'
 import icon from '../assets/icon.jpg'
 
 function CategorySlide() {
     const slides = [
-        {member2},
-        {member2},
-        {member2},
-        {member2},{member2},{member2},{member2},{member2},{member2},
+        member2,
+        icon,
+        member2,
+        icon,member2,member2,icon,icon,member2
     ];
 
     return (
@@ -20,18 +19,16 @@ function CategorySlide() {
             <Swiper
                 slidesPerView={8}
                 spaceBetween={30}
-                freeMode={true}
                 pagination={{
                     clickable: true,
                 }}
-                modules={[FreeMode, Pagination]}
+                modules={[Pagination]}
                 className="mySwiper"
-                navigation={true}
             >
             {slides.map((src, index) => (
                     <SwiperSlide key={index}>
                         <div className="item rounded-[10px] bg-[#787878] overflow-hidden">
-                            <img src={member2} alt={`Banner slide ${index + 1}`} className="w-full rounded-full h-auto object-cover p-2" />
+                            <img src={src} alt={`Banner slide ${index + 1}`} className="w-full rounded-full h-auto object-cover p-2" />
                         </div>
                     </SwiperSlide>
                 ))}
